59660e664d7a83a0dbe6170d131ea564642d92eb
Sihyeong <tlgud2325@naver.com>
2021-08-03 20:31:03 +0900

    [FIX] detail

--- posts/urls.py
+++ posts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name="post_list"),
+    path('')
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 from .forms import PostForm
 from django.views.decorators.csrf import csrf_exempt
@@ -10,4 +10,36 @@ def post_list(request):
     ctx = {
         "posts": posts
     }
-    return render(request, 'posts/post_list.html', ctx)
+    return render(request, """'html 넣어주세요'""", ctx)
+
+
+def post_detail(request, pk):
+    posts = Post.objects.get(pk=pk)
+    # 댓글기능도 끌어와야함.
+    ctx = {
+        "posts": posts
+        # 여기에도 댓글 넣어주어야함.
+    }
+    return render(request, """'html 넣어주세요'""", ctx)
+
+
+def post_create(request):
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            posts = form.save()
+            posts.save()
+            return redirect("""'url 넣어주세요'""")
+    else:
+        form = PostForm()
+        ctx = {
+            "form": form,
+        }
+
+        return render(request, """'html 넣어주세요'""", ctx)
+
+
+def post_delete(request, pk):
+    posts = Post.objects.get(pk=pk)
+    posts.delete()
+    return redirect("""'url 넣어주세요'""")
