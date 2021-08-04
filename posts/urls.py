654390cf4d0a9fc21ee876c68b6fefd6b4edd449
whdrnjs96 <whdrnjs96@gmail.com>
2021-08-04 23:46:59 +0900

    [ADD] add post_detail.html

--- posts/urls.py
+++ posts/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "posts"
 urlpatterns = [
     path("", views.post_list, name="post_list"),
-    path("detail<int:pk>/", views.post_detail, name="post_detail"),
+    path("detail/<int:pk>/", views.post_detail, name="post_detail"),
     path("create/", views.post_create, name="post_create"),
     path("<int:pk>/update", views.post_update, name="post_update"),
     path("<int:pk>/delete", views.post_delete, name="post_delete"),
