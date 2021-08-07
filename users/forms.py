2704477741dcf2cabfe2d55d0972b1d459582d7a
Bean <whddkrak@gmail.com>
2021-08-07 20:47:34 +0900

    [ADD] signup error message custom

--- users/forms.py
+++ users/forms.py
@@ -6,11 +6,83 @@ from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import get_user, get_user_model
 from django.contrib.auth.forms import UserChangeForm
+import re
 
 # 회원 가입을 위한 form
 class UserCustomCreationForm(UserCreationForm):
+    #placeholder
+    username= forms.CharField(widget=forms.TextInput
+                        (attrs={'placeholder':'사용할 아이디를 입력해 주세요'}))
+    email = forms.CharField(widget=forms.TextInput
+                        (attrs={'placeholder': '사용할 이메일을 입력해 주세요'}))
+    user_nickname = forms.CharField(widget=forms.TextInput
+                        (attrs={'placeholder': '사용할 닉네임을 입력해주세요(최대8자)'}))
+    password1 = forms.CharField(widget=forms.TextInput
+                        (attrs={'placeholder': '사용할 비밀번호를 입력해주세요'}))
+    password2 = forms.CharField(widget=forms.TextInput
+                        (attrs={'placeholder': '비밀번호를 재입력해주세요'}))
+
+    #eror_messages custom
+    error_messages = {
+        'existed_id': '이미 존재하는 아이디 입니다.',
+        'existed_username': '이미 존재하는 닉네임입니다.',
+        'discorded_password': '비밀번호가 불일치 합니다.',
+        'password_format_error': '6자 이상 입력해 주세요.',
+        'email_format_error': '이메일 형식이 잘못됐습니다.'
+    }
+
     class Meta:
         #model = User
         model = get_user_model()
         fields = ("username","email", "user_nickname", "password1", "password2")
+    
+    # 이미 존재하는 아이디 검사
+    def clean_username(self):
+        input_username = self.cleaned_data.get("username")
+        if User.objects.filter(username=input_username):
+            raise forms.ValidationError(
+                self.error_messages['existed_id'],
+                code='existed_id'
+            )
+        return input_username
 
+    # 이미 존재하는 아이디 검사
+    def clean_email(self):
+        valided_email = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+        input_email = self.cleaned_data.get("email")
+        if input_email and valided_email.match(input_email) != False:
+            raise forms.ValidationError(
+                self.error_messages['email_format_error'],
+                code='email_format_error'
+            )
+        return input_email
+    
+    #이미 존재하는 닉네임 검사
+    def clean_user_nickname(self):
+        input_nickname = self.cleaned_data.get("user_nickname")
+        if User.objects.filter(user_nickname=input_nickname):
+            raise forms.ValidationError(
+                self.error_messages['existed_username'],
+                code='existed_username'
+            )
+        return input_nickname
+
+    # password1 != password2 검사
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(
+                self.error_messages['discorded_password'],
+                code='discorded_password',
+            )
+        #비밀번호 format검사
+        if password1 and password2 and password1 == password2:
+            if len(str(password1)) < 6 and len(str(password2)) < 6:
+                raise forms.ValidationError(
+                    self.error_messages['password_format_error'],
+                    code='password_format_error',
+                )
+        return password2
+    
+    
