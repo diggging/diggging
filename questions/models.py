ea5987be4b4c9c5b232fd885383183d001a86269
whdrnjs96 <whdrnjs96@gmail.com>
2021-08-08 23:29:39 +0900

    [ADD] question_detail temp file 1

--- questions/models.py
+++ questions/models.py
@@ -8,6 +8,10 @@ from core import models as core_models
 class Question_post(posts_models.Post):
     sand_point = models.IntegerField(default=0)
 
+    def sand_point_count(self):
+        return self.sand_point.count()
+    sand_point_count.short_description = "current sand point"
+
 
 class Answer(core_models.TimeStampModel):
     user = models.ForeignKey(
@@ -15,3 +19,6 @@ class Answer(core_models.TimeStampModel):
     )
     selection = models.BooleanField(verbose_name="채택", default=False)
     desc = models.TextField(blank=False)
+
+class QuestionFolder(posts_models.Folder):
+    pass
