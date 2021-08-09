016ea60f703698f334a64beeea039bd2fb7cfd13
whdrnjs96 <whdrnjs96@gmail.com>
2021-08-09 10:29:12 +0900

    [FIX] email-verification error & login.html change

--- users/views.py
+++ users/views.py
@@ -83,7 +83,7 @@ def activate(request, uidb64, token):
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
-        login(request, user)
+        login(request, user, backend='django.contrib.auth.backends.ModelBackend')
         return HttpResponse('Thank you for your email confirmation. Now you can login your account')
     else:
         return HttpResponse('Activation link is invalid!')
