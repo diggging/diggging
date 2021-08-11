3901cc877a5eda23086943c1ca2d751fb09a2b54
whdrnjs96 <whdrnjs96@gmail.com>
2021-08-11 18:04:27 +0900

    [FIX] add null True to Comment model

--- comments/models.py
+++ comments/models.py
@@ -4,17 +4,21 @@ from core import models as core_models
 # Create your models here.
 class Comment(core_models.TimeStampModel):
     post = models.ForeignKey(
-        "posts.Post", on_delete=models.CASCADE, related_name="comments"
+        "posts.Post", on_delete=models.CASCADE, related_name="comments", blank=True, null=True,
     )
     question = models.ForeignKey(
         "questions.Question_post",
         on_delete=models.CASCADE,
         related_name="question_comments",
+        blank=True,
+        null=True,
     )
     answer = models.ForeignKey(
         "questions.Answer",
         on_delete=models.CASCADE,
         related_name="answer_comments",
+        blank=True,
+        null=True,
     )
     user = models.ForeignKey(
         "users.User", on_delete=models.CASCADE, related_name="comments"
