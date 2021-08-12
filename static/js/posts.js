6e7854017f883b8af8f0523c4b017a7e3fdab610
Bean <whddkrak@gmail.com>
2021-08-12 21:16:10 +0900

    [FIX]  input창 reset시 목록 모두 뜨는 현상 수정

--- static/js/posts.js
+++ static/js/posts.js
@@ -3,7 +3,6 @@ const listUser = []; //user data 추출용
 const postList = []; // post fields -> 여기에 post, username까지 들어감.
 const userList = []; // user fields
 
-const test = '{{ posts.user }}'
 
 //post data
 const getPost = async() => {
@@ -37,52 +36,60 @@ const getUser = async() => {
     }
 }
 
-
-
-
+// 검색 관련 함수
 function numberWithCommas(x) { //숫자 3자리마다  ,
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
+// 검색어 match
 function findMatches(wordToMatch, postList) {
     return postList.filter(post => {
         const regex = new RegExp(wordToMatch, 'gi') //패턴을 사용해 텍스트를 판별할 때 사용하는 정규 표현식 객체를 생성
-        return post.title.match(regex) || post.desc.match(regex) || post.tag.match(regex)
+        return post.title.match(regex) 
+        || post.desc.match(regex) 
+        || post.tag.match(regex)
     });
 }
 
+// matching된 검색 관련 글 출력
 function displayMatches(){
-    const matchArray = findMatches(this.value, postList, postIdList);
+    const matchArray = findMatches(this.value, postList);
     const html = matchArray.map(post => {
-        const regex = new RegExp(this.value, 'gu');
-        const title = post.title.replace(regex, this.value);
-        const desc = post.desc.replace(regex, this.value);
-        const tag = post.tag.replace(regex, this.value);
+        const regex = new RegExp(this.value, 'gi');
+        const title = post.title.replace(regex, `<span class="h1">${this.value}</span>`);
+        const desc = post.desc.replace(regex, `<span class="h1">${this.value}</span>`);
+        const tag = post.tag.replace(regex, `<span class="h1">${this.value}</span>`);
 
         return `
             <div>
                 ${title}
-                ${desc}
-                ${tag}
             </div>
                 `
     }).join('');
-    suggestions.innerHTML = html;
+
+    // not empty
+    if (checkEmpty.value && // if exist AND
+        checkEmpty.value.length > 0 && // if value have one charecter at least
+        checkEmpty.value.trim().length > 0 )// if value is not just spaces
+    {   
+        suggestions.innerHTML = html;
+        console.log('value is:    ' + checkEmpty.value);
+    }
+    // empty
+    else if(!checkEmpty.value) {
+        suggestions.innerHTML = "";
+        console.log('No value!');
+    } 
 }
 
+const checkEmpty = document.querySelector('.search');
 const searchInput = document.querySelector('.search');
 const suggestions = document.querySelector('.post_container');
 
-function init() {
-    getPost()
-    searchInput.addEventListener('change', displayMatches);
-    searchInput.addEventListener('keyup', displayMatches); //타이핑 할때마다.
-    // searchInput.addEventListener("input", displayInputValue);
+searchInput.addEventListener('input', displayMatches);
 
+function init() {
+    getPost();
+    // checkInput();
 }
 init();
-
-// <li>
-// <span class="name">${title}</span>
-// <span class="population">${numberWithCommas(post.title)}</span>
-// </li>
