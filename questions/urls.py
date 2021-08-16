dce07f9ebd66ec1f9a11c312d05e71c67cd32525
Bean <whddkrak@gmail.com>
2021-08-17 01:40:33 +0900

    [FIX] a tag url

--- questions/urls.py
+++ questions/urls.py
@@ -14,10 +14,8 @@ urlpatterns = [
     path("<int:question_post_id>/<answer_id>/answer_delete", views.answer_delete, name="answer_delete"),
     path("<int:question_post_id>/get_question", views.get_question, name="get_question"),
     path("<int:question_answer_id>/chosen_answer", views.chosen_answer, name="chosen_answer"),
-    path("like/", views.question_like, name="question_like"),
-    path("scrap/", views.question_scrap, name="question_scrap"),
+
 
     path("<int:answer_id>/get_answer_comments", views.get_answer_comments, name="get_answer_comments"),
-    path("answer_ajax/", views.answer_ajax, name="answer_ajax"),
 ]
 
