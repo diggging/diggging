b057c9f35c7e3e7574ca34b15e6df6a98b9bf6c9
whdrnjs96 <whdrnjs96@gmail.com>
2021-08-17 05:30:08 +0900

    [FIX] Post 대대적 공사

--- posts/models.py
+++ posts/models.py
@@ -1,11 +1,5 @@
 from django.db import models
-from django.db.models.fields import BooleanField, CharField
-from django.db.models.fields.related import ForeignKey
-from django.urls import reverse
-from users.models import User
 from core import models as core_models
-from tagging.fields import TagField
-from ckeditor.fields import RichTextField
 from ckeditor_uploader.fields import RichTextUploadingField
 from django.conf import settings
 
--- posts/urls.py
+++ posts/urls.py
@@ -3,12 +3,10 @@ from . import views
 
 app_name = "posts"
 urlpatterns = [
-    # path("", views.post_list, name="post_list"),
     path("main/", view=views.main, name="main"),  # main 페이지로 가는 api
     path("main/helped/", views.helped, name="helped"),
     path("main/follow/", views.follow, name="follow"),
     path("main/my_recent/", views.my_recent, name="my_recent"),
-    # path("detail/<int:pk>/", views.post_detail, name="post_detail"), (original ver)
     path(
         "<int:user_id>/<int:post_id>/detail", views.post_detail, name="post_detail"
     ),  # 처음 <int:pk> 의 pk: user의 id
@@ -16,12 +14,14 @@ urlpatterns = [
     path("create/", views.post_create, name="post_create"),
     path("<int:pk>/update", views.post_update, name="post_update"),
     path("<int:pk>/delete", views.post_delete, name="post_delete"),
+
     path("search/", views.search, name="search"),
-    path("<int:user_id>/<int:post_id>/get_post", view=views.get_post, name="get_post"),
-    #path("count_like_scrap/", views.count_like_scrap, name="count_like_scrap"),
     path("search_post_axios/", views.search_post_axios, name="search_post_axios"),
     path("search_user_axios/", views.search_user_axios, name="search_user_axios"),
+    path("search_input/", views.search_input, name="search_input"),
+    path("search_select/", views.search_select, name="search_select"),
 
+    path("<int:user_id>/<int:post_id>/get_post", view=views.get_post, name="get_post"),
     path("scrap_axios/", views.scrap_axios, name="scrap_axios"),
     path("helped_axios/", views.helped_axios, name="helped_axios"),
     path("follow_axios/", views.follow_axios, name="follow_axios"),
@@ -29,12 +29,6 @@ urlpatterns = [
 
     path("like/", views.post_like, name="post_like"),
     path("scrap/", views.post_scrap, name="post_scrap"),
-    # path("count_like_scrap/", views.count_like_scrap, name="count_like_scrap"),
-    # path("folder/", views.make_folder, name="make_folder"),
-
-    path("search_input/", views.search_input, name="search_input"),
-    path("search_select/", views.search_select, name="search_select"),
-
     #test
     path("test/", views.test, name="test"),
 ]
