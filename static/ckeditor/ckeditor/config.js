c32028d2f8f4d99878c6f4868303d241f0899d53
hyunju <gkrry27232@naver.com>
2021-08-20 07:24:55 +0000

    kakao share fix

--- static/ckeditor/ckeditor/config.js
+++ static/ckeditor/ckeditor/config.js
@@ -7,4 +7,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+	config.allowedContent = true;
 };
+
+CKEDITOR.config.autoParagraph = false;
