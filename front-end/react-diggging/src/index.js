6a05994ef2189aa33fcef4e5026dacbbc89328e2
Bean <whddkrak@gmail.com>
2021-09-26 01:31:44 +0900

    react-firebase 찐연동

---
+++ front-end/react-diggging/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import firebase from './firebase';
+
+console.log(firebase);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
