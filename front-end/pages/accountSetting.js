def075c97bb8cb7eca15f4d0b9947600f7fb0640
JisuKim <jso_osj@naver.com>
2021-11-04 16:15:15 +0900

    [add] page추가: 계정설정 및 비밀번호페이지

---
+++ front-end/pages/accountSetting.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function accountSetting() {
+  return (
+    <div>
+      비밀번호 변경하기
+      <input />
+      <input />
+      <input />
+    </div>
+  );
+}
+
+export default accountSetting;
---
+++ front-end/pages/reset-password-confirm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function reset_password_confirm() {
+  return <div>비밀번호 변경이 완료되었습니다.</div>;
+}
+
+export default reset_password_confirm;
