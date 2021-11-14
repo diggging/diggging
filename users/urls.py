410799fb68cab3c7b253392050b0bd5f7549f37a
sihyeong <tlgud2325@naver.com>
2021-11-14 13:41:23 +0900

    [Update] Add  userinfo api

--- users/urls.py
+++ users/urls.py
@@ -9,6 +9,7 @@ app_name = "users"
 urlpatterns = [
     path("api/Signup/", views.Registration.as_view(), name="Signup"),
     path("api/Login/", views.Login.as_view(), name="Login"),
+    path("api/userinfo/", views.LoadUserView.as_view(), name="userinfo"),
     # path("login", obtain_auth_token, name="login"),
     # url(r'^signup/$', views.signup, name='signup'),
     path("activate/<slug:uidb64>/<slug:token>/", views.activate, name="activate"),
--- users/views.py
+++ users/views.py
@@ -197,6 +197,21 @@ class Login(generics.GenericAPIView):
         )
 
 
+# 유저정보 api
+class LoadUserView(APIView):
+    def get(self, request, format=None):
+        try:
+            user = request.user
+            user = UserSerializer(user)
+
+            return Response({"user": user.data}, status=status.HTTP_200_OK)
+        except:
+            return Response(
+                {"error": "Something went wrong when trying to load user"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+
+
 """@csrf_exempt
 def log_in(request):
     context = {}
