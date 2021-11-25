6ed4152f4a468c0b115a35c28bd4cd513238f6df
Bean <whddkrak@gmail.com>
2021-11-26 00:50:34 +0900

    [FIX] _app.js ' ; ' delete

--- front-end/pages/_app.js
+++ front-end/pages/_app.js
@@ -61,7 +61,7 @@ function MyApp({ Component, pageProps }) {
         </Head>
         <GlobalStyles />
         <ThemeProvider theme={colors}>
-          <Component {...pageProps} />;
+          <Component {...pageProps} />
         </ThemeProvider>
       </Provider>
     </>
