5122655d7816819a199138c612dbe5b5079aa40e
JisuKim <jso_osj@naver.com>
2021-12-04 23:25:06 +0900

    [add]svg component files

--- front-end/public/static/images/ToggleBtn.js
+++ front-end/public/static/images/ToggleBtn.js
@@ -1,17 +1,15 @@
 import * as React from "react";
 
-function SvgToggleBtn(props) {
-  return (
-    <svg
-      width={12}
-      height={9}
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      {...props}
-    >
-      <path d="M6 9L.804 0h10.392L6 9z" fill="#C4C4C4" />
-    </svg>
-  );
-}
+const SvgToggleBtn = (props) => (
+  <svg
+    width={12}
+    height={9}
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path d="M6 9 .804 0h10.392L6 9Z" fill="#C4C4C4" />
+  </svg>
+);
 
 export default SvgToggleBtn;
--- front-end/public/static/images/index.js
+++ front-end/public/static/images/index.js
@@ -1,5 +1,6 @@
-export { default as Alarm } from './Alarm'
-export { default as Diggging } from './Diggging'
-export { default as Directory } from './Directory'
-export { default as Search } from './Search'
-export { default as ToggleBtn } from './ToggleBtn'
+export { default as Alarm } from "./Alarm";
+export { default as BookMarkIcon } from "./BookMarkIcon";
+export { default as Diggging } from "./Diggging";
+export { default as Directory } from "./Directory";
+export { default as Search } from "./Search";
+export { default as ToggleBtn } from "./ToggleBtn";
