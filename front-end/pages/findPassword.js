95a995b3889856b2c06f0a680cb6b627425842f2
JisuKim <jso_osj@naver.com>
2021-12-08 15:38:17 +0900

    [Add]이메일 인증완료 , 비번찾기 페이지 ui끝
    비번찾기 이메일전송 api연결안됨 : 404error

--- front-end/pages/findPassword.js
+++ front-end/pages/findPassword.js
@@ -5,6 +5,9 @@ import Layout from '../hocs/Layout';
 import styled from 'styled-components';
 import NavBar from '../components/NavBar';
 import GreyInput from '../components/common/GreyInput';
+import YellowButton from "../components/common/YellowButton";
+import YellowTitle from '../components/common/YellowTitle';
+
 
 function findPassword() {
   const [email, setEmail] = useState('');
@@ -13,17 +16,17 @@ function findPassword() {
     setEmail(e.target.value);
   }
 
-  
+  //API주소 과연맞는가... 404에러뜬다
   const findPassword = async (e, email) => {
     e.preventDefault();
-    await axios.post(`http://localhost:3000/users/api/password_reset_done`, {
+    await axios.post(`${API_URL}/users/api/password_reset_done`, {
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'text/plain',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'POST',
-        'Access-Control-Max-Age': 3600,
-        'Access-Control-Allow-Headers': origin,
+        // 'Access-Control-Allow-Origin': '*', //cors에러때문에 넣어본 것들...
+        // 'Access-Control-Allow-Methods': 'POST',
+        // 'Access-Control-Max-Age': 3600,
+        // 'Access-Control-Allow-Headers': origin,
       },
       credentials: "include",
       email: email
@@ -41,18 +44,20 @@ function findPassword() {
         <PageTitle>비밀번호 찾기</PageTitle>
         <GuideMessage>가입하신 이메일을 입력하시면 해당 주소로 비밀번호 변경 링크를 보내드립니다.</GuideMessage>
         <form onSubmit={findPassword} type="POST">
-        <iframe name="iframe"></iframe>
-        <GreyInput
-          width="28.75rem"
-          height="3.125rem"
-          type="email"
-          name="email"
-          placeholder="이메일"
-          onChange={onInput}
-          value={email}
-          required
-        />
-        <button>전송</button>
+          <YellowTitle fontSize="1.375rem" >이메일</YellowTitle>
+          <GreyInput
+            width="28.75rem"
+            height="3.125rem"
+            marginRight="2.875rem"
+            marginLeft="2.75rem"
+            type="email"
+            name="email"
+            placeholder="이메일"
+            onChange={onInput}
+            value={email}
+            required
+          />
+          <YellowButton paddingTop="0.9375rem" paddingRight="2.1875rem" >전송</YellowButton>
         </form>
       </FormBox>
     </>
@@ -65,7 +70,7 @@ const FormBox = styled.section`
   margin-top: 11.25rem;
 `;
 
-const PageTitle = styled.h1`
+const PageTitle = styled.span`
   font-family: 'Pretendard-Bold';
   font-size: 1.625rem;
   color: #343434;
@@ -74,11 +79,14 @@ const PageTitle = styled.h1`
 `;
 
 const GuideMessage = styled.p`
-  display: inline-block;
+  display: block;
   color: #8D8C85;
   font-size: 1.125rem;
   font-family: "Pretendard-Medium";
   border-top: solid 2px #E5E5E5;
+  margin-top: 95px;
+  padding-top: 20px;
+  margin-bottom: 40px;
 `;
 
 export {FormBox, PageTitle, GuideMessage};
