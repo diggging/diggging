9a678c6026460af5d0dd52eab7ac41bb39039ad9
whdrnjs96 <whdrnjs96@gmail.com>
2021-12-12 13:44:42 +0900

    [FIX] base settings

--- shoveling/settings/base.py
+++ shoveling/settings/base.py
@@ -91,7 +91,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.TokenAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         #"rest_framework.permissions.IsAuthenticated",
